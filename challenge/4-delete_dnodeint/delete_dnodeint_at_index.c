b3cda030b982e222557630c372d4a2f77f08db49
JohnLopezCabrera <9043@Holbertonstudents.com>
2024-09-09 11:19:51 -0400

    l

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,50 +11,43 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current;
-    unsigned int p;
+	dlistint_t *saved_head;
+	dlistint_t *tmp;
+	unsigned int p;
 
-    if (*head == NULL)  /* Empty list */
-    {
-        return (-1);
-    }
-
-    current = *head;
-    p = 0;
-
-    /* Traverse the list to find the node at the given index */
-    while (p < index && current != NULL)
-    {
-        current = current->next;
-        p++;
-    }
-
-    if (current == NULL)  /* Index out of range */
-    {
-        return (-1);
-    }
-
-    /* Deleting the head node */
-    if (index == 0)
-    {
-        *head = current->next;  /* Move head to the next node */
-        if (*head != NULL)
-        {
-            (*head)->prev = NULL;  /* Set new head's prev to NULL */
-        }
-    }
-    else  /* Deleting a node that is not the head */
-    {
-        if (current->prev != NULL)
-        {
-            current->prev->next = current->next;  /* Link previous node to next */
-        }
-        if (current->next != NULL)
-        {
-            current->next->prev = current->prev;  /* Link next node to previous */
-        }
-    }
-
-    free(current);  /* Free the node */
-    return (1);
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	saved_head = *head;
+	p = 0;
+	while (p < index && *head != NULL)
+	{
+		*head = (*head)->next;
+		p++;
+	}
+	if (p != index)
+	{
+		*head = saved_head;
+		return (-1);
+	}
+	if (0 == index)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+		if (tmp != NULL)
+		{
+			tmp->prev = NULL;
+		}
+	}
+	else
+	{
+		(*head)->prev->next = (*head)->next;
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		free(*head);
+		*head = saved_head;
+	}
+	return (1);
 }
